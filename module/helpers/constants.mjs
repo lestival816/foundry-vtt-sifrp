25e84af7ae26169a61be17fd16dd299f2f295ca4
Lesti <lestival11@gmail.com>
2026-01-10 16:36:15 +0100

    chore : setting to 0.1.2

--- module/helpers/constants.mjs
+++ module/helpers/constants.mjs
@@ -3,7 +3,7 @@ export const SIFRP_CONSTANTS = {}
 SIFRP_CONSTANTS.Settings = {
     SYSTEM_NAME: "sifrp",
     SYSTEM_NAME_LONG: "A Song of Ice anf Fire Roleplaying (Unofficial)",
-    CURRENT_VERSION: "0.1.1",
+    CURRENT_VERSION: "0.1.2",
     DEBUG_LOGS: "debugLogs"
 }
 
